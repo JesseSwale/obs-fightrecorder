ea6a5cb15e3bbab669f93bb88a6b799a87a01c7b
Jesse Swale <56308993+JesseSwale@users.noreply.github.com>
2025-06-29 15:28:21 +0200

    Check for Eve clients

--- src/plugin-main.h
+++ src/plugin-main.h
@@ -29,6 +29,7 @@ typedef struct fightrecorder_data {
 	bool active;
 	bool concatdelete;
 	bool concat;
+	bool replaybuffer_alwayson;
 	const char *logs_dir;
 	const char *logs_regex;
 	const char *output_dir;
@@ -37,6 +38,7 @@ typedef struct fightrecorder_data {
 
 	// state
 	bool started_recording;
+	bool active_eve_clients; // only used when replaybuffer_alwayson = false
 	pthread_t thread_id;
 } fightrecorder_data_t;
 
@@ -68,4 +70,3 @@ void *dummy_source_create(obs_data_t *settings, obs_source_t *source);
 void on_obs_frontend_event_exit();
 bool obs_module_load(void);
 void on_obs_frontend_event_finished_loading();
-bool replay_buffer_settings_modified_cb(obs_properties_t *props, obs_property_t *prop, obs_data_t *settings);
